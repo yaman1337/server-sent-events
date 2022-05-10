5e02abf17bbbff55baebf846e32130a4b2705bf4
Yaman Sarabariya <yaman.sarabariya@gmail.com>
2022-05-10 19:11:23 +0545

    send data to specific client

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (_,res) => res.sendFile("/index.html"));
+app.get("/", (_, res) => res.sendFile("/index.html"));
 
-let i = 0 ;
+let i = 0;
 
-app.get("/streams", (req,res) => {
-    res.setHeader("Content-Type", "text/event-stream");
-    sendEventStreams(res);
-})
+app.get("/streams", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  sendEventStreams(res);
+});
+
+app.get("/private-stream", (req, res) => {
+  const { username } = req.query;
+  res.setHeader("Content-type", "text/event-stream");
+  setInterval(() => res.write(`data: Sent to ${username} \n\n`), 2000);
+});
 
 function sendEventStreams(res) {
-    res.write("data: " + `Event stream ${i} \n\n`);
-     setTimeout(_ => sendEventStreams(res), 1000)
+  res.write("data: " + `Event stream ${i} \n\n`);
+  setTimeout((_) => sendEventStreams(res), 1000);
 }
 
-app.listen(process.env.port || 9000 , () => {
-    console.log("listening in port 9000")
-})
+app.listen(process.env.port || 9000, () => {
+  console.log("listening in port 9000");
+});
